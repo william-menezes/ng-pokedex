7ae454fea83b8ff997a32d36ec12fa68cf89b818
william-menezes <williamenezes@outlook.com>
2022-09-23 13:01:01 -0300

    fix: mocking up data

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonCardComponent implements OnInit {
 
+  @Input() pokemons: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.pokemons)
   }
 
 }
