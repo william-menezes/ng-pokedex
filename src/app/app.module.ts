d7205fa3fa82626e25de6b2a9d5a123f4ead6758
William <williamenezes@outlook.com>
2022-09-24 00:33:01 -0300

    feat: adding pokemon-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonCardComponent
+    PokemonCardComponent,
+    PokemonDetailsComponent
   ],
   imports: [
     BrowserModule,
