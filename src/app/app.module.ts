82c99fcb74da2271d3721b7af27d09270aa7d37d
William Damascena <williamenezes@outlook.com>
2022-12-06 00:19:28 -0300

    feat: adding header and search-bar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
-import { HomeComponent } from './pages/home/home.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,12 @@ import { DetailsComponent } from './pages/details/details.component';
     PokemonCardComponent,
     PokemonDetailsComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    HeaderComponent,
+    SearchBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent implements OnInit {
+  input = new FormControl('');
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
