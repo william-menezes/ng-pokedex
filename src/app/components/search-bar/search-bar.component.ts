e1d25b32f3d8e729743fe6d61c74bd43d60af87e
William Menezes Damascena <williamenezes@outlook.com>
2025-05-04 02:12:05 -0300

    feat: implementing search-bar search

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +8,20 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() searchEmitter: EventEmitter<string | null> = new EventEmitter();
+
   input = new FormControl('');
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  search() {
+    this.searchEmitter.emit(this.input.value);
+  }
+
+  clearInput() {
+    this.input.reset();
+    this.searchEmitter.emit(this.input.value);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,65 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { NamedApiResource } from 'src/app/models/named-api-resource';
 
+import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from './../../service/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   @Output() pokemonsList: NamedApiResource[] = [];
+  hasError: boolean = false;
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
     this.getPokemons();
   }
 
   getPokemons(): void {
-    this.pokemonService.getAllPokemons().subscribe(response => {
-      this.pokemonsList = response.results;
+    this.pokemonService.getAllPokemons().subscribe((pokemonList) => {
+      this.pokemonsList = pokemonList.results;
+      console.log(this.pokemonsList);
     });
   }
 
+  getPokemonByName(name: string | null) {
+    if (name) {
+      this.pokemonService.getPokemonByName(name).subscribe(
+        (pokemon) => {
+          this.pokemonsList = [];
+          this.pokemonsList.push(this.transformPokemonToNamedResource(pokemon));
+        },
+        (error) => {
+          this.hasError = true;
+          setTimeout(() => {
+            this.close();
+          }, 4000);
+        }
+      );
+    } else {
+      if (this.pokemonsList.length == 1) {
+        this.getPokemons();
+      } else {
+        return;
+      }
+    }
+  }
+
+  transformPokemonToNamedResource(pokemon: Pokemon) {
+    const namedApiResource: NamedApiResource = {
+      name: pokemon.name,
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`,
+      status: pokemon,
+    };
+
+    return namedApiResource;
+  }
+
+  close() {
+    this.hasError = false;
+  }
 }
